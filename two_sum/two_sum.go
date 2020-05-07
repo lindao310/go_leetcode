4fe1537b9ccc8ba1b84896af8c63292ed3d99d41
吴道林 <wu.daolin@immomo.com>
2020-05-07 14:05:55 +0800

    two sum, another two solution

--- two_sum/two_sum.go
+++ two_sum/two_sum.go
@@ -1,5 +1,7 @@
 package two_sum
 
+import "fmt"
+
 //暴力解法 时间复杂福O(n^2)
 func twoSum(nums []int, target int) []int {
 	for i, val := range nums {
@@ -11,4 +13,43 @@ func twoSum(nums []int, target int) []int {
 	}
 
 	return nil
+}
+
+//两遍hash 时间复杂福O(n)
+func twoSum2(nums []int, target int) []int {
+	hash := make(map[int]int)
+
+	//先存一遍
+	for j, num := range nums {
+		hash[num] = j
+	}
+
+	fmt.Println(hash)
+
+	for i, val := range nums {
+		another := target - val
+		index, ok := hash[another] //读一遍
+		if ok && (index != i) {
+			return []int{i,index}
+		}
+	}
+
+	return []int{0,0}
+}
+
+//两遍hash 时间复杂福O(n)
+func twoSum3(nums []int, target int) []int {
+	hash := make(map[int]int)
+
+	for i, val := range nums {
+		another := target - val
+		index, ok := hash[another] //读一遍
+		if ok && (index != i) {
+			return []int{index, i} //hash exist index before current i
+		}
+
+		hash[val] = i
+	}
+
+	return []int{0,0}
 }
--- two_sum/two_sum_test.go
+++ two_sum/two_sum_test.go
@@ -7,13 +7,15 @@ import (
 
 func TestTwoSum(t *testing.T) {
 
-	nums := []int{2,7,11,15,2}
+	nums := []int{3,2,7,11,15,2}
 
-	target := 4
+	target := 5
 
-	result := []int{0,4}
+	result := []int{0,1}
 
-	ret := twoSum(nums, target)
+	//ret := twoSum(nums, target)
+	//ret := twoSum2(nums, target) //has bug,in this case,return [0,5]
+	ret := twoSum3(nums, target)
 
 	fmt.Println("twoSum return ", ret)
 
